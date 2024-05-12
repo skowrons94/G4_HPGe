2479f32e35a354ced32bc124dc0fe93bbd0dc432
Jakub Skowronski <jakub.skowronski@protonmail.com>
2024-05-12 11:40:53 +0200

    Update

--- Run.py
+++ Run.py
@@ -6,35 +6,42 @@ import subprocess
 file = sys.argv[1]
 
 x, y, z, d = 0, 0, 0, 7
-iters = 2
 
-for i in range( iters ):
+# Create a histo for x and y
+xbins = np.arange(-3, 3.1, 0.3)
+ybins = np.arange(-3, 3.1, 0.3)
 
-    # Wait for 2 seconds
-    subprocess.run(["sleep", "2"])
-
-    # Sample y from a uniform distribution between -3 and 3
-    x = np.random.uniform(-3, 3)
-    # Sample z from a uniform distribution between -3 and 3
-    y = np.random.uniform(-3, 3)
-
-    with open(file, 'r') as f:
-        lines = f.readlines()
-        for i, line in enumerate(lines):
-            if "xxx" in line:
-                lines[i] = lines[i].replace("xxx", str(x))
-            if "yyy" in line:
-                lines[i] = lines[i].replace("yyy", str(y))
-            if "zzz" in line:
-                lines[i] = lines[i].replace("zzz", str(z))
-            if "ddd" in line:
-                lines[i] = lines[i].replace("ddd", str(d))
+for x in xbins:
     
-    with open("mac/run.mac", 'w') as f:
-        f.writelines(lines)
+    for y in ybins:
 
-    # Get source name from file
-    source = file.split("/")[-1].split(".")[0]
+        x, y = round(x, 1), round(y, 1) 
 
-    subprocess.run(["./G4_HPGe", "mac/run.mac"])
-    subprocess.run(["mv", "sim.root", "{}_{}_{}.root".format(source, x, y)])
+        # Wait for 2 seconds
+        subprocess.run(["sleep", "2"])
+
+        # Sample y from a uniform distribution between -3 and 3
+        x = np.random.uniform(-3, 3)
+        # Sample z from a uniform distribution between -3 and 3
+        y = np.random.uniform(-3, 3)
+
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            for i, line in enumerate(lines):
+                if "xxx" in line:
+                    lines[i] = lines[i].replace("xxx", str(x))
+                if "yyy" in line:
+                    lines[i] = lines[i].replace("yyy", str(y))
+                if "zzz" in line:
+                    lines[i] = lines[i].replace("zzz", str(z))
+                if "ddd" in line:
+                    lines[i] = lines[i].replace("ddd", str(d))
+    
+        with open("mac/run.mac", 'w') as f:
+            f.writelines(lines)
+
+        # Get source name from file
+        source = file.split("/")[-1].split(".")[0]
+
+        subprocess.run(["./G4_HPGe", "mac/run.mac"])
+        subprocess.run(["mv", "sim.root", "{}_{}_{}.root".format(source, x, y)])
