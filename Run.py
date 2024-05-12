370d9246a6c03244fc16d4647ebf498cc4aa2ea7
Jakub Skowronski <jakub.skowronski@protonmail.com>
2024-05-12 11:50:31 +0200

    Debug

--- Run.py
+++ Run.py
@@ -43,5 +43,5 @@ for x in xbins:
         # Get source name from file
         source = file.split("/")[-1].split(".")[0]
 
-        subprocess.run(["./G4_HPGe", "mac/run.mac"])
+        subprocess.run(["./G4_HPGe", "mac/run.mac"], timeout=600)
         subprocess.run(["mv", "sim.root", "{}_{}_{}.root".format(source, x, y)])
